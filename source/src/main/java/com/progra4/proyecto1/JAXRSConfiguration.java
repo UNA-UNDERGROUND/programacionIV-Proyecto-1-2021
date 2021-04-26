31087209a90f3e5100c02b8220e5d06e5efb7539
ninjaoflight <jonguz5@hotmail.com>
2021-04-26 14:15:40 -0600

    eliminada clase sin uso

--- source/src/main/java/com/progra4/proyecto1/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.progra4.proyecto1;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
