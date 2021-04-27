a45ed5c21431a73999b10dd0f92237ca875f2431
ninjaoflight <jonguz5@hotmail.com>
2021-04-27 12:47:51 -0600

    agregadas pruebas unitarias Estado

--- source/src/test/java/com/progra4/proyecto1/DAO/TestEstadoDAO.java
+++ source/src/test/java/com/progra4/proyecto1/DAO/TestEstadoDAO.java
@@ -2,34 +2,57 @@ package com.progra4.proyecto1.DAO;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.progra4.proyecto1.Controlador;
 import com.progra4.proyecto1.modelo.beans.Estado;
 import com.progra4.proyecto1.modelo.dao.EstadoDAO;
 
 public class TestEstadoDAO {
-	Controlador controlador = Controlador.getInstancia();
+	EstadoDAO dao = new EstadoDAO();
+	Estado e1 = new Estado(-1, "Estado de prueba");
+	Estado e2 = new Estado(-2, "Estado de prueba");
+	Estado e3 = new Estado(-2, "Estado de prueba");
 
 	@Test
 	public void agregarEstadoTest() {
 		Boolean exitoso = false;
-		Estado e = new Estado(1, "Estado de prueba");
-		exitoso = new EstadoDAO().add(1, e);
-		assertEquals(exitoso, true);
+		exitoso = dao.add(-1, e1);
+		dao.delete(-1);
+		assertTrue(exitoso);
 	}
 
-	// Usuario creds = new Usuario(208010443, "asdf1234", false);
-	// Cuenta origen = controlador.recuperarCuenta(1);
-	//
-	// boolean res = controlador.agregarTransferencia(origen,2, new
-	// BigDecimal("1000"),"Prueba Limite Transferencia(Backend)",creds);
-	//
-	// if(res){
-	// System.out.println("exitoso");
-	// }
-	// else{
-	// System.out.println("fallido");
-	// }
+	@Test
+	public void listarEstados() {
+		dao.add(-2, new Estado(-2, "Estado de prueba"));
+		int conteo = dao.listAll().size();
+		dao.delete(-2);
+		assertTrue(conteo > 0);
+	}
 
+	@Test
+	public void recuperarEstadoTest() {
+		dao.add(-3, new Estado(-3, "Estado de prueba"));
+		Estado val = dao.retrieve(-3);
+		dao.delete(-3);
+		assertNotEquals(val, null);
+	}
+
+	@Test
+	public void actualizarEstadoTest() {
+		Estado val = new Estado(-5, "actualizado");
+		dao.add(-4, val);
+		dao.update(-4, val);
+		val = dao.retrieve(-5);
+		dao.delete(-5);
+		assertTrue(!val.equals(null) && val.getDescripcion().equals("actualizado"));
+	}
+
+	@Test
+	public void eliminarEstadoTest() {
+		Boolean exitoso = false;
+		dao.add(-6, new Estado(-6, "actualizado"));
+		exitoso = dao.delete(-6);
+		assertTrue(exitoso);
+	}
 }
