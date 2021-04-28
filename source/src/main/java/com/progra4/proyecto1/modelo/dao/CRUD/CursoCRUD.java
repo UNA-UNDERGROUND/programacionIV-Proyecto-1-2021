b1a0e72d01501568ae6e9b877168e156ab16cd81
ninjaoflight <jonguz5@hotmail.com>
2021-04-28 16:04:02 -0600

    agregados CRUDS

---
+++ source/src/main/java/com/progra4/proyecto1/modelo/dao/CRUD/CursoCRUD.java
@@ -0,0 +1,62 @@
+package com.progra4.proyecto1.modelo.dao.CRUD;
+
+import com.progra4.proyecto1.utils.dao.crud.AbstractCRUD;
+
+public class CursoCRUD extends AbstractCRUD {
+    
+    @Override
+    public String getListAllCmd() {
+        return LIST_CMD;
+    }
+
+    @Override
+    public String getAddCmd() {
+        return ADD_CMD;
+    }
+
+    @Override
+    public String getRetrieveCmd() {
+        return RETRIEVE_CMD;
+    }
+
+    @Override
+    public String getUpdateCmd() {
+        return UPDATE_CMD;
+    }
+
+    @Override
+    public String getDeleteCmd() {
+        return DELETE_CMD;
+    }
+
+	protected static final String PARAMS_LIST 
+		= "id_curso, descripcion, area_tematica_id ";
+    protected static final String TABLE_NAME
+        = "curso ";
+    protected static final String ID_FIELD
+        = "id_curso ";
+	protected static final String UPDATE_PARAMS
+        = "id_curso = ?, descripcion = ?, area_tematica_id = ? ";
+    protected static final String VALUES_STRING
+        = "?, ?, ? ";
+
+	protected static final String LIST_CMD
+		= "SELECT "
+		+ PARAMS_LIST
+		+ "FROM eif209_2021_01." + TABLE_NAME + " ORDER BY " + ID_FIELD +"; ";
+	protected static final String ADD_CMD
+		= "INSERT INTO eif209_2021_01." + TABLE_NAME
+		+ "(" + PARAMS_LIST + ") "
+		+ "VALUES (" + VALUES_STRING + "); ";
+	protected static final String RETRIEVE_CMD
+		= "SELECT "
+		+ PARAMS_LIST
+		+ "FROM eif209_2021_01." + TABLE_NAME + " WHERE " + ID_FIELD + " = ?; ";
+	protected static final String UPDATE_CMD
+		= "UPDATE eif209_2021_01." + TABLE_NAME 
+		+ "SET " + UPDATE_PARAMS + " "
+		+ "WHERE " + ID_FIELD +" = ?; ";
+	protected static final String DELETE_CMD
+		= "DELETE FROM eif209_2021_01." + TABLE_NAME
+		+ "WHERE " + ID_FIELD +" = ?; ";
+}
