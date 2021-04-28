76230f6ded31c3e5411831112b62b44ae20af578
ninjaoflight <jonguz5@hotmail.com>
2021-04-28 10:35:58 -0600

    refactorizado crud

--- source/src/main/java/com/progra4/proyecto1/modelo/dao/CRUD/EstadoCRUD.java
+++ source/src/main/java/com/progra4/proyecto1/modelo/dao/CRUD/EstadoCRUD.java
@@ -29,24 +29,30 @@ public class EstadoCRUD extends AbstractCRUD {
         return DELETE_CMD;
     }
 
+    protected static final String PARAMS_LIST 
+        = "id_estado, descripcion " ;
+    protected static final String TABLE_NAME
+        = "estado ";
+    protected static final String ID_FIELD
+        = "id_estado ";
     protected static final String LIST_CMD
             = "SELECT "
-            + "id_estado, descripcion "
-            + "FROM eif209_2021_01.estado ORDER BY id_estado; ";
+            + PARAMS_LIST
+            + "FROM eif209_2021_01." + TABLE_NAME + " ORDER BY " + ID_FIELD +"; ";
     
     protected static final String ADD_CMD
-            = "INSERT INTO eif209_2021_01.estado "
-            + "(id_estado, descripcion) "
+            = "INSERT INTO eif209_2021_01." + TABLE_NAME
+            + "(" + PARAMS_LIST + ") "
             + "VALUES (?, ?); ";
     protected static final String RETRIEVE_CMD
             = "SELECT "
-            + "id_estado, descripcion "
-            + "FROM eif209_2021_01.estado WHERE id_estado = ?; ";
+            + PARAMS_LIST
+            + "FROM eif209_2021_01." + TABLE_NAME + " WHERE " + ID_FIELD + " = ?; ";
     protected static final String UPDATE_CMD
-            = "UPDATE eif209_2021_01.estado "
+            = "UPDATE eif209_2021_01." + TABLE_NAME 
             + "SET id_estado = ?, descripcion = ? "
-            + "WHERE id_estado = ?; ";
+            + "WHERE " + ID_FIELD +" = ?; ";
     protected static final String DELETE_CMD
-            = "DELETE FROM eif209_2021_01.estado "
-            + "WHERE id_estado = ?; ";
+            = "DELETE FROM eif209_2021_01." + TABLE_NAME
+            + "WHERE " + ID_FIELD +" = ?; ";
 }
